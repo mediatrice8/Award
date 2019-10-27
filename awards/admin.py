3964707d6fb6ec05fba7967d6f22ca94c0c750c0
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-27 17:12:02 +0200

    tasks of admin on dashboard

--- awards/admin.py
+++ awards/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Comments, Profile, Projects
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Projects)
+admin.site.register(Comments)
