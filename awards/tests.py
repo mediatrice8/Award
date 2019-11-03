c16dc352981f9864cdb19b83f12cde542065e5a2
mediatrice8 <kampiremediatrice@gmail.com>
2019-11-03 22:57:16 +0200

    test for isolated code

--- awards/tests.py
+++ awards/tests.py
@@ -37,3 +37,54 @@ class ProfileTest(TestCase):
         self.assertTrue(self.updated_profile.bio,'web designer')
         
         
+class projectsTest(TestCase):
+    def setUp(self):
+        self.user=User(username='hono',email='hoza@gmail.com')
+        self.user.save()
+        self.new_profile=Profile(user=self.user,phone="536382210",bio='developer')
+        self.new_profile.save()
+        self.new_project = Projects(user=self.user,link="https://ig20.herokuapp.com/")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_project,Projects))
+
+    def test_data(self):
+        self.assertTrue(self.new_project.link,"https://news-8-app.herokuapp.com/")
+
+    def test_save(self):
+        self.new_project.save()
+        projects = Projects.objects.all()
+        self.assertTrue(len(projects)>0)
+
+    def test_delete(self):
+        project = Projects.objects.filter(id=1)
+        project.delete()
+        projects = Projects.objects.all()
+        self.assertTrue(len(projects)==0)
+
+    def test_update_project(self):
+        self.new_project.save()
+        self.update_project = Projects.objects.filter(link='https://ig20.herokuapp.com/').update(link = 'https://news-8-app.herokuapp.com/')
+        self.updated_project = Projects.objects.get(link='https://news-8-app.herokuapp.com/')
+        self.assertTrue(self.updated_post.link,'https://news-8-app.herokuapp.com/')
+
+class CommentTest(TestCase):
+    def setUp(self):
+        self.new_user=User(username='hono',email='hoza@gmail.com')
+        self.new_user.save()
+        self.new_profile=Profile(user=self.new_user,phone="536382210",bio='developer')
+        self.new_profile.save()
+        self.new_project = Projects(user=self.new_user,link='https://www.google.com')
+        self.new_project.save()
+        self.comment=Comments(user=self.new_user,project=self.new_project,comment='nice one')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comment,Comments))
+
+    def test_data(self):
+        self.assertTrue(self.comment.comment,"nice one")
+
+    def test_comments(self):
+        self.comment.save()
+        comment=Comments.objects.all()
+        self.assertTrue(len(comment)>0)
