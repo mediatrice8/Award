63c21895729b35311d8c020ded1ab9f94bcc5f8c
mediatrice8 <kampiremediatrice@gmail.com>
2019-11-06 17:45:28 +0200

    deployment

--- awardapp/settings.py
+++ awardapp/settings.py
@@ -199,7 +199,7 @@ STATICFILES_DIRS = (
 ##########################
 
 # Simplified static file serving.
-https://warehouse.python.org/project/whitenoise/
+# https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
